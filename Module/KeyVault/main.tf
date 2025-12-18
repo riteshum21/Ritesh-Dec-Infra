3110975dee3dcd4a74426ba5d07bb609285127ba
riteshum21 <ritesh.shukl8085@gmail.com>
2025-12-18 22:04:06 +0530

    G

--- Module/KeyVault/main.tf
+++ Module/KeyVault/main.tf
@@ -22,7 +22,7 @@ for_each = var.keyvault
       "Get",  "Create", "List",
     ]
     secret_permissions = [
-      "Get",  "Set", "List", "Delete",
+      "Get",  "Set", "List", "Delete", "Purge", "Recover"
     ]
     storage_permissions =[
       "Get",   "List",
